e652e1dcf8f60a0392bf8d124be12456d759c39d
Mamed766 <mehemmeddemirli57@gmail.com>
2024-09-21 15:18:42 +0400

    pagination

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -20,7 +20,7 @@ import {
   Checkbox,
 } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-import { FaRegHeart } from "react-icons/fa";
+import { FaAngleLeft, FaAngleRight, FaRegHeart } from "react-icons/fa";
 import Image from "next/image";
 import axios from "axios";
 
@@ -33,10 +33,21 @@ const Page = () => {
 
   const [filteredData, setFilteredData] = useState([]);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+  const [paginationInfo, setPaginationInfo] = useState({
+    totalCollections: 0,
+    totalPages: 0,
+    currentPage: 1,
+    limit: itemsPerPage,
+  });
+
   const fetchFilteredData = async () => {
     try {
       const collectionTypeParam =
-        collectionType.length > 0 ? collectionType.join(",") : null;
+        collectionType.length > 0 && !collectionType.includes("All")
+          ? collectionType.join(",")
+          : null;
 
       const colorParam = color.length > 0 ? color.join(",") : null;
       const materialParam = material ? material : null;
@@ -49,9 +60,12 @@ const Page = () => {
           collectionType: collectionTypeParam,
           minPrice: 0,
           maxPrice: maxPriceParam,
+          page: currentPage,
+          limit: itemsPerPage,
         },
       });
       setFilteredData(response.data.collections);
+      setPaginationInfo(response.data.pagination);
     } catch (error) {
       console.error("Error fetching filtered data:", error);
     }
@@ -59,9 +73,15 @@ const Page = () => {
 
   useEffect(() => {
     fetchFilteredData();
+  }, [price, material, color, collectionType, currentPage]);
+
+  useEffect(() => {
+    setCurrentPage(1);
   }, [price, material, color, collectionType]);
 
-  console.log(filteredData);
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <>
@@ -93,7 +113,6 @@ const Page = () => {
       <div className="max-w-[1280px] mx-auto mt-20 flex gap-2">
         <aside className="max-w-[15rem] min-w-[15rem]">
           <Accordion allowMultiple>
-            {/* 1. Accordion - Collection */}
             <AccordionItem>
               <h2>
                 <AccordionButton>
@@ -107,12 +126,10 @@ const Page = () => {
                 <CheckboxGroup
                   onChange={(value: string[]) => {
                     if (value.includes("All")) {
-                      // "All" seçildiyse, diğer filtreleri sıfırla
                       setCollectionType(["All"]);
                       setMaterial("");
                       setColor([]);
                     } else {
-                      // "All" seçili değilse
                       setCollectionType(value);
                     }
                   }}
@@ -127,7 +144,6 @@ const Page = () => {
               </AccordionPanel>
             </AccordionItem>
 
-            {/* 2. Accordion - Price */}
             <AccordionItem>
               <h2>
                 <AccordionButton>
@@ -141,7 +157,7 @@ const Page = () => {
                 <Box mb={4}>Price: ${price}</Box>
                 <Slider
                   aria-label="slider-ex-1"
-                  defaultValue={50}
+                  defaultValue={price}
                   min={0}
                   max={5000}
                   onChangeEnd={(val) => setPrice(val)}
@@ -154,7 +170,6 @@ const Page = () => {
               </AccordionPanel>
             </AccordionItem>
 
-            {/* 3. Accordion - Material */}
             <AccordionItem>
               <h2>
                 <AccordionButton>
@@ -183,7 +198,6 @@ const Page = () => {
               </AccordionPanel>
             </AccordionItem>
 
-            {/* 4. Accordion - Colors */}
             <AccordionItem>
               <h2>
                 <AccordionButton>
@@ -214,53 +228,90 @@ const Page = () => {
             </AccordionItem>
           </Accordion>
         </aside>
-        <div className="flex flex-wrap gap-2">
-          {filteredData.length > 0 ? (
-            filteredData.map((collection: any, index: number) => {
-              return (
-                <div key={index} className="relative group overflow-hidden">
-                  <Image
-                    alt={collection.title}
-                    src={`http://localhost:3001/${collection?.image}`}
-                    height={300}
-                    width={300}
-                    quality={100}
-                    objectFit="cover"
-                    className="object-cover cursor-pointer"
-                  />
-                  <div className="absolute cursor-pointer group-hover:right-5 top-3 right-[-4rem] bg-white p-3 rounded-full duration-300">
-                    <FaRegHeart />
+        <div className="flex flex-col flex-1">
+          <div className="flex flex-wrap gap-5">
+            {filteredData.length > 0 ? (
+              filteredData.map((collection: any, index: number) => {
+                return (
+                  <div
+                    key={index}
+                    className="relative group overflow-hidden w-[300px]"
+                  >
+                    <Image
+                      alt={collection.title}
+                      src={`http://localhost:3001/${collection?.image}`}
+                      height={300}
+                      width={300}
+                      quality={100}
+                      objectFit="cover"
+                      className="object-cover cursor-pointer"
+                    />
+                    <div className="absolute cursor-pointer group-hover:right-5 top-3 right-[-4rem] bg-white p-3 rounded-full duration-300">
+                      <FaRegHeart />
+                    </div>
+                    <div className="absolute flex flex-col gap-2 font-medium text-[12px] top-2 left-2 text-start">
+                      {collection?.subscription && (
+                        <p className="bg-orange-400 px-2">Subscription</p>
+                      )}
+                      {collection.percent && (
+                        <p className="bg-red-500 px-2 text-white max-w-[50px]">
+                          -{collection?.percent}%
+                        </p>
+                      )}
+                      {collection?.new && (
+                        <p className="bg-blue-500 px-2 text-white max-w-[50px]">
+                          New
+                        </p>
+                      )}
+                      {collection?.hot && (
+                        <p className="bg-blue-600 px-2 text-white max-w-[50px]">
+                          HOT
+                        </p>
+                      )}
+                    </div>
+                    <div className="text-center">
+                      <h2 className="text-[#555]">{collection?.title}</h2>
+                      <p className="font-semibold">{collection?.desc}</p>
+                      <p>${collection?.price}</p>
+                    </div>
                   </div>
-                  <div className="absolute flex flex-col gap-2 font-medium text-[12px] top-2 left-2 text-start">
-                    {collection?.subscription && (
-                      <p className="bg-orange-400 px-2">Subscription</p>
-                    )}
-                    {collection.percent && (
-                      <p className="bg-red-500 px-2 text-white max-w-[50px]">
-                        -{collection?.percent}%
-                      </p>
-                    )}
-                    {collection?.new && (
-                      <p className="bg-blue-500 px-2 text-white max-w-[50px]">
-                        New
-                      </p>
-                    )}
-                    {collection?.hot && (
-                      <p className="bg-blue-600 px-2 text-white max-w-[50px]">
-                        HOT
-                      </p>
-                    )}
-                  </div>
-                  <div className="text-center">
-                    <h2 className="text-[#555]">{collection?.title}</h2>
-                    <p className="font-semibold">{collection?.desc}</p>
-                    <p>${collection?.price}</p>
-                  </div>
-                </div>
-              );
-            })
-          ) : (
-            <p>No products found.</p>
+                );
+              })
+            ) : (
+              <p>No products found.</p>
+            )}
+          </div>
+
+          {paginationInfo.totalPages > 1 && (
+            <div className="flex justify-center mt-6">
+              <button
+                onClick={() => handlePageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+                className="px-3 py-1 mx-1  rounded disabled:opacity-50"
+              >
+                <FaAngleLeft />
+              </button>
+              {Array.from({ length: paginationInfo.totalPages }, (_, index) => (
+                <button
+                  key={index}
+                  onClick={() => handlePageChange(index + 1)}
+                  className={`px-3 py-1 mx-1  ${
+                    paginationInfo.currentPage === index + 1
+                      ? "bg-white-500 border-2 text-black border-black "
+                      : "bg-white"
+                  }`}
+                >
+                  {index + 1}
+                </button>
+              ))}
+              <button
+                onClick={() => handlePageChange(currentPage + 1)}
+                disabled={currentPage === paginationInfo.totalPages}
+                className="px-3 py-1 mx-1 bg-white-500  text-black  disabled:opacity-50"
+              >
+                <FaAngleRight />
+              </button>
+            </div>
           )}
         </div>
       </div>
